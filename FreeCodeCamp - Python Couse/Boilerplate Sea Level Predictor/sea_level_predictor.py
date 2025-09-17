a3a93dc8bc6256da97ca8d6ae104ff7d9fc946b2
Carlos Icaro <carlos.paiva@sou.inteli.edu.br>
2025-09-17 00:24:09 -0300

    feat: Adiciona Sea Level Predictor

---
+++ FreeCodeCamp - Python Couse/Boilerplate Sea Level Predictor/sea_level_predictor.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+
+def draw_plot():
+    # Read data from file   
+    df = pd.read_csv("epa-sea-level.csv")
+
+    # Create scatter plot
+    x = df["Year"]
+    y = df["CSIRO Adjusted Sea Level"]
+    df.plot.scatter(x, y, c='cor', colormap='viridis', title='Scatter Plot com Pandas')
+    plt.show()
+
+    # Create first line of best fit
+
+
+    # Create second line of best fit
+
+
+    # Add labels and title
+
+    
+    # Save plot and return data for testing (DO NOT MODIFY)
+    plt.savefig('sea_level_plot.png')
+    return plt.gca()
